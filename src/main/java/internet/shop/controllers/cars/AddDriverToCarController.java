24dd923cec4b609efe5dba5351ccbf7d32b4b205
gigbat <b.kostevich@gmail.com>
2021-01-21 19:42:08 +0200

    renamed add_driver_to_car.jsp

--- src/main/java/internet/shop/controllers/cars/AddDriverToCarController.java
+++ src/main/java/internet/shop/controllers/cars/AddDriverToCarController.java
@@ -21,7 +21,7 @@ public class AddDriverToCarController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/cars/addDriverToCar.jsp")
+        req.getRequestDispatcher("/WEB-INF/views/cars/add_driver_to_car.jsp")
                 .forward(req, resp);
     }
 
